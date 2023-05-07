c1f1b641e78647b886346f655ae0c75317169a8c
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2023-05-07 12:30:50 +0900

    09. React-Router

--- practice/09.react-router/Games.jsx
+++ practice/09.react-router/Games.jsx
@@ -1,20 +1,24 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import NumberBaseball from '../03.숫자야구/NumberBaseballClass';
-import RSP from '../05.가위바위보/RSPClass';
-import Lotto from '../06.로또 추첨기/LottoClass';
-import GameMatcherClass from './GameMatcherClass';
+import React from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import GameMatcher from './GameMatcher';
+
 const Games = () => {
   return (
     <BrowserRouter>
       <div>
         <Link to="/game/number-baseball">숫자야구</Link>
+        &nbsp;
         <Link to="/game/rock-scissors-paper">가위바위보</Link>
-        <Link to="/game/lotto-generator">로또 생성기</Link>
+        &nbsp;
+        <Link to="/game/lotto-generator">로또생성기</Link>
+        &nbsp;
         <Link to="/game/index">게임 매쳐</Link>
       </div>
       <div>
-        <Route path="/game/:name" component={GameMatcherClass}></Route>
+        <Routes>
+          <Route path="/" element={<GameMatcher />} />
+          <Route path="/game/*" element={<GameMatcher />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
